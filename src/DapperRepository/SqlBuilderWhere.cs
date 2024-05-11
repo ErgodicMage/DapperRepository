9638363bbe634b0b8c682e547db364cc07563481
ErgodicMage <ErgodicMage@gmail.com>
2024-05-10 22:16:08 -0500

    Added Fluent API for Where clauses for SqlBuilderWhere.

--- src/DapperRepository/SqlBuilderWhere.cs
+++ src/DapperRepository/SqlBuilderWhere.cs
@@ -13,77 +13,14 @@ public abstract class SqlBuilderWhere : SqlBuilder
     public IList<Where>? WhereConditions { get; set; }
     #endregion
 
-    #region Fluent
-    public SqlBuilderWhere AddWhere(Where where)
+    #region Add Where
+    public void AddWhere(Where where)
     {
         WhereConditions ??= new List<Where>();
         WhereConditions.Add(where);
-        return this;
     }
 
-    public SqlBuilderWhere AddWhereId()
-    {
-        _whereId = _mapper.GetWhereId();
-        return this;
-    }
-
-    public SqlBuilderWhere AddWhereEqual(ColumnMapper column) => AddWhere(new Where(column, WhereOperator.Equals));
-    public SqlBuilderWhere AddAndWhereEqual(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.And, column, WhereOperator.Equals));
-    public SqlBuilderWhere AddOrWhereEqual(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.Or, column, WhereOperator.Equals));
-    public SqlBuilderWhere AddWhereEqual(string columnName) => AddWhere(new Where(columnName, WhereOperator.Equals));
-    public SqlBuilderWhere AddAndWhereEqual(string columnName) => AddWhere(new Where(WhereAndOrNot.And, columnName, WhereOperator.Equals));
-    public SqlBuilderWhere AddOrWhereEqual(string columnName) => AddWhere(new Where(WhereAndOrNot.Or, columnName, WhereOperator.Equals));
-
-    public SqlBuilderWhere AddWhereNotEqual(ColumnMapper column) => AddWhere(new Where(column, WhereOperator.NotEquals));
-    public SqlBuilderWhere AddAndWhereNotEqual(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.And, column, WhereOperator.NotEquals));
-    public SqlBuilderWhere AddOrWhereNotEqual(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.Or, column, WhereOperator.NotEquals));
-    public SqlBuilderWhere AddWhereNotEqual(string columnName) => AddWhere(new Where(columnName, WhereOperator.NotEquals));
-    public SqlBuilderWhere AddAndWhereNotEqual(string columnName) => AddWhere(new Where(WhereAndOrNot.And, columnName, WhereOperator.NotEquals));
-    public SqlBuilderWhere AddOrWhereNotEqual(string columnName) => AddWhere(new Where(WhereAndOrNot.Or, columnName, WhereOperator.NotEquals));
-
-
-    public SqlBuilderWhere AddWhereNull(ColumnMapper column) => AddWhere(new Where(column, WhereOperator.Null));
-    public SqlBuilderWhere AddAndWhereNull(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.And, column, WhereOperator.Null));
-    public SqlBuilderWhere AddOrWhereNull(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.Or, column, WhereOperator.Null));
-    public SqlBuilderWhere AddWhereNull(string columnName) => AddWhere(new Where(columnName, WhereOperator.Null));
-    public SqlBuilderWhere AddAndWhereNull(string columnName) => AddWhere(new Where(WhereAndOrNot.And, columnName, WhereOperator.Null));
-    public SqlBuilderWhere AddOrWhereNull(string columnName) => AddWhere(new Where(WhereAndOrNot.Or, columnName, WhereOperator.Null));
-
-    public SqlBuilderWhere AddWhereNotNull(ColumnMapper column) => AddWhere(new Where(column, WhereOperator.NotNull));
-    public SqlBuilderWhere AddAndWhereNotNull(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.And, column, WhereOperator.NotNull));
-    public SqlBuilderWhere AddOrWhereNotNull(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.Or, column, WhereOperator.NotNull));
-    public SqlBuilderWhere AddWhereNotNull(string columnName) => AddWhere(new Where(columnName, WhereOperator.NotNull));
-    public SqlBuilderWhere AddAndWhereNotNull(string columnName) => AddWhere(new Where(WhereAndOrNot.And, columnName, WhereOperator.NotNull));
-    public SqlBuilderWhere AddOrWhereNotNull(string columnName) => AddWhere(new Where(WhereAndOrNot.Or, columnName, WhereOperator.NotNull));
-
-    public SqlBuilderWhere AddWhereGreaterThan(ColumnMapper column) => AddWhere(new Where(column, WhereOperator.GreaterThan));
-    public SqlBuilderWhere AddAndWhereGreaterThan(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.And, column, WhereOperator.GreaterThan));
-    public SqlBuilderWhere AddOrWhereGreaterThan(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.Or, column, WhereOperator.GreaterThan));
-    public SqlBuilderWhere AddWhereGreaterThan(string columnName) => AddWhere(new Where(columnName, WhereOperator.GreaterThan));
-    public SqlBuilderWhere AddAndWhereGreaterThan(string columnName) => AddWhere(new Where(WhereAndOrNot.And, columnName, WhereOperator.GreaterThan));
-    public SqlBuilderWhere AddOrWhereGreaterThan(string columnName) => AddWhere(new Where(WhereAndOrNot.Or, columnName, WhereOperator.GreaterThan));
-
-    public SqlBuilderWhere AddWhereGreaterThanOrEqual(ColumnMapper column) => AddWhere(new Where(column, WhereOperator.GreaterThanOrEqual));
-    public SqlBuilderWhere AddAndWhereGreaterThanOrEqual(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.And, column, WhereOperator.GreaterThanOrEqual));
-    public SqlBuilderWhere AddOrWhereGreaterThanOrEqual(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.Or, column, WhereOperator.GreaterThanOrEqual));
-    public SqlBuilderWhere AddWhereGreaterThanOrEqual(string columnName) => AddWhere(new Where(columnName, WhereOperator.GreaterThanOrEqual));
-    public SqlBuilderWhere AddAndWhereGreaterThanOrEqual(string columnName) => AddWhere(new Where(WhereAndOrNot.And, columnName, WhereOperator.GreaterThanOrEqual));
-    public SqlBuilderWhere AddOrWhereGreaterThanOrEqual(string columnName) => AddWhere(new Where(WhereAndOrNot.Or, columnName, WhereOperator.GreaterThanOrEqual));
-
-    public SqlBuilderWhere AddWhereLessThan(ColumnMapper column) => AddWhere(new Where(column, WhereOperator.LessThan));
-    public SqlBuilderWhere AddAndWhereLessThan(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.And, column, WhereOperator.LessThan));
-    public SqlBuilderWhere AddOrWhereLessThan(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.Or, column, WhereOperator.LessThan));
-    public SqlBuilderWhere AddWhereLessThan(string columnName) => AddWhere(new Where(columnName, WhereOperator.LessThan));
-    public SqlBuilderWhere AddAndWhereLessThan(string columnName) => AddWhere(new Where(WhereAndOrNot.And, columnName, WhereOperator.LessThan));
-    public SqlBuilderWhere AddOrWhereLessThan(string columnName) => AddWhere(new Where(WhereAndOrNot.Or, columnName, WhereOperator.LessThan));
-
-    public SqlBuilderWhere AddWhereLessThanOrEqual(ColumnMapper column) => AddWhere(new Where(column, WhereOperator.LessThanOrEqual));
-    public SqlBuilderWhere AddAndWhereLessThanOrEqual(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.And, column, WhereOperator.LessThanOrEqual));
-    public SqlBuilderWhere AddOrWhereLessThanOrEqual(ColumnMapper column) => AddWhere(new Where(WhereAndOrNot.Or, column, WhereOperator.LessThanOrEqual));
-    public SqlBuilderWhere AddWhereLessThanOrEqual(string columnName) => AddWhere(new Where(columnName, WhereOperator.LessThanOrEqual));
-    public SqlBuilderWhere AddAndWhereLessThanOrEqual(string columnName) => AddWhere(new Where(WhereAndOrNot.And, columnName, WhereOperator.LessThanOrEqual));
-    public SqlBuilderWhere AddOrWhereLessThanOrEqual(string columnName) => AddWhere(new Where(WhereAndOrNot.Or, columnName, WhereOperator.LessThanOrEqual));
-
+    public void AddWhereId() => _whereId ??= _mapper.GetWhereId();
     #endregion
 
     #region Build Where Conditions
@@ -101,12 +38,6 @@ public abstract class SqlBuilderWhere : SqlBuilder
 
         if (WhereConditions is null || WhereConditions.Count == 0) return;
 
-        if (!string.IsNullOrEmpty(_whereId))
-        {
-            sb.Append(_whereId);
-            return;
-        }
-
         bool first = true;
         foreach (var condition in WhereConditions)
         {
@@ -123,7 +54,5 @@ public abstract class SqlBuilderWhere : SqlBuilder
             first = false;
         }
     }
-
-
     #endregion
 }
--- src/DapperRepository/SqlBuilderWhereFluentExtensions.cs
+++ src/DapperRepository/SqlBuilderWhereFluentExtensions.cs
@@ -1,5 +1,123 @@
-﻿namespace ErgodicMage.DapperRepository;
+﻿using System.Runtime.CompilerServices;
+
+namespace ErgodicMage.DapperRepository;
 
 public static class SqlBuilderWhereFluentExtensions
 {
+    public static SqlBuilderWhere Where(this SqlBuilderWhere builder, Where where)
+    {
+        builder.AddWhere(where);
+        return builder;
+    }
+
+    public static SqlBuilderWhere WhereId(this SqlBuilderWhere builder)
+    {
+        builder.AddWhereId();
+        return builder;
+    }
+
+    public static SqlBuilderWhere WhereEqual(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(column, WhereOperator.Equals));
+    public static SqlBuilderWhere AndWhereEqual(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.And, column, WhereOperator.Equals));
+    public static SqlBuilderWhere OrWhereEqual(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.Or, column, WhereOperator.Equals));
+    public static SqlBuilderWhere WhereEqual(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(columnName, WhereOperator.Equals));
+    public static SqlBuilderWhere AndWhereEqual(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.And, columnName, WhereOperator.Equals));
+    public static SqlBuilderWhere OrWhereEqual(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.Or, columnName, WhereOperator.Equals));
+
+    public static SqlBuilderWhere AddWhereNotEqual(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(column, WhereOperator.NotEquals));
+    public static SqlBuilderWhere AddAndWhereNotEqual(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.And, column, WhereOperator.NotEquals));
+    public static SqlBuilderWhere AddOrWhereNotEqual(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.Or, column, WhereOperator.NotEquals));
+    public static SqlBuilderWhere AddWhereNotEqual(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(columnName, WhereOperator.NotEquals));
+    public static SqlBuilderWhere AddAndWhereNotEqual(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.And, columnName, WhereOperator.NotEquals));
+    public static SqlBuilderWhere AddOrWhereNotEqual(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.Or, columnName, WhereOperator.NotEquals));
+
+    public static SqlBuilderWhere AddWhereNull(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(column, WhereOperator.Null));
+    public static SqlBuilderWhere AddAndWhereNull(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.And, column, WhereOperator.Null));
+    public static SqlBuilderWhere AddOrWhereNull(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.Or, column, WhereOperator.Null));
+    public static SqlBuilderWhere AddWhereNull(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(columnName, WhereOperator.Null));
+    public static SqlBuilderWhere AddAndWhereNull(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.And, columnName, WhereOperator.Null));
+    public static SqlBuilderWhere AddOrWhereNull(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.Or, columnName, WhereOperator.Null));
+
+    public static SqlBuilderWhere AddWhereNotNull(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(column, WhereOperator.NotNull));
+    public static SqlBuilderWhere AddAndWhereNotNull(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.And, column, WhereOperator.NotNull));
+    public static SqlBuilderWhere AddOrWhereNotNull(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.Or, column, WhereOperator.NotNull));
+    public static SqlBuilderWhere AddWhereNotNull(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(columnName, WhereOperator.NotNull));
+    public static SqlBuilderWhere AddAndWhereNotNull(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.And, columnName, WhereOperator.NotNull));
+    public static SqlBuilderWhere AddOrWhereNotNull(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.Or, columnName, WhereOperator.NotNull));
+
+    public static SqlBuilderWhere AddWhereGreaterThan(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(column, WhereOperator.GreaterThan));
+    public static SqlBuilderWhere AddAndWhereGreaterThan(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.And, column, WhereOperator.GreaterThan));
+    public static SqlBuilderWhere AddOrWhereGreaterThan(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.Or, column, WhereOperator.GreaterThan));
+    public static SqlBuilderWhere AddWhereGreaterThan(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(columnName, WhereOperator.GreaterThan));
+    public static SqlBuilderWhere AddAndWhereGreaterThan(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.And, columnName, WhereOperator.GreaterThan));
+    public static SqlBuilderWhere AddOrWhereGreaterThan(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.Or, columnName, WhereOperator.GreaterThan));
+
+    public static SqlBuilderWhere AddWhereGreaterThanOrEqual(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(column, WhereOperator.GreaterThanOrEqual));
+    public static SqlBuilderWhere AddAndWhereGreaterThanOrEqual(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.And, column, WhereOperator.GreaterThanOrEqual));
+    public static SqlBuilderWhere AddOrWhereGreaterThanOrEqual(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.Or, column, WhereOperator.GreaterThanOrEqual));
+    public static SqlBuilderWhere AddWhereGreaterThanOrEqual(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(columnName, WhereOperator.GreaterThanOrEqual));
+    public static SqlBuilderWhere AddAndWhereGreaterThanOrEqual(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.And, columnName, WhereOperator.GreaterThanOrEqual));
+    public static SqlBuilderWhere AddOrWhereGreaterThanOrEqual(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.Or, columnName, WhereOperator.GreaterThanOrEqual));
+
+    public static SqlBuilderWhere AddWhereLessThan(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(column, WhereOperator.LessThan));
+    public static SqlBuilderWhere AddAndWhereLessThan(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.And, column, WhereOperator.LessThan));
+    public static SqlBuilderWhere AddOrWhereLessThan(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.Or, column, WhereOperator.LessThan));
+    public static SqlBuilderWhere AddWhereLessThan(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(columnName, WhereOperator.LessThan));
+    public static SqlBuilderWhere AddAndWhereLessThan(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.And, columnName, WhereOperator.LessThan));
+    public static SqlBuilderWhere AddOrWhereLessThan(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.Or, columnName, WhereOperator.LessThan));
+
+    public static SqlBuilderWhere AddWhereLessThanOrEqual(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(column, WhereOperator.LessThanOrEqual));
+    public static SqlBuilderWhere AddAndWhereLessThanOrEqual(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.And, column, WhereOperator.LessThanOrEqual));
+    public static SqlBuilderWhere AddOrWhereLessThanOrEqual(this SqlBuilderWhere builder, ColumnMapper column) 
+        => builder.Where(new Where(WhereAndOrNot.Or, column, WhereOperator.LessThanOrEqual));
+    public static SqlBuilderWhere AddWhereLessThanOrEqual(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(columnName, WhereOperator.LessThanOrEqual));
+    public static SqlBuilderWhere AddAndWhereLessThanOrEqual(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.And, columnName, WhereOperator.LessThanOrEqual));
+    public static SqlBuilderWhere AddOrWhereLessThanOrEqual(this SqlBuilderWhere builder, string columnName) 
+        => builder.Where(new Where(WhereAndOrNot.Or, columnName, WhereOperator.LessThanOrEqual));
+
 }
